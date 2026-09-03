e21c421816080a1aa926c9d20f73bde56cba401d
Duke-soccer2510 <flashlightening25@icloud.com>
2026-09-03 14:36:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const message = document.getElementById("message");
 button.addEventListener("click", () => {
     message.textContent = "⚽ Welcome to the Soccer Website!";
 });
+const tabs = document.querySelectorAll(".wc-tab");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+
+        tabs.forEach(t => t.classList.remove("active"));
+
+        tab.classList.add("active");
+
+        alert(tab.textContent + " selected!");
+    });
+});
